914ea3085207aa02c42dbb203a09fb130e74f517
Vlnduwfsfrekves <112093090+Vlnduwfsfrekves@users.noreply.github.com>
2024-10-24 01:17:50 +0300

    -n

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,48 @@
-abstract class Key{
-    id:number
-    constructor(id:number){
-    this.id=id
-  }
+abstract class House{
+    protected door:boolean=false
+    protected key:Key
+    protected tenants:Person[]=[]
+    constructor(key:Key){
+        this.key=key
+    }
+    comeIn(person:Person):void{
+        if (this.door){
+            this.tenants.push(person)
+        }
+    }
+    abstract openDoor(key:Key):void
+}
+
+class MyHouse extends House{
+    openDoor(key:Key):void{
+        if(this.key.getSignature()===key.getSignature()){
+            this.door=true
+        }
+    }
+}
+
+class Key{
+    private signature:number
+    constructor(){
+        this.signature=Math.floor(Math.random()*1000)
+    }
+    getSignature():number{
+        return this.signature
+    }
 }
 
 class Person{
-  name:string
-  constructor(name: string) {
-    this.name = name;
-  }
-}
-
-abstract class House {
-  protected door: boolean = false
-  protected key: Key;
-  protected tenants: Person[] = []
-  constructor(key: Key) {
-    this.key = key;
-  }
-  comeIn(person: Person): void {
-    if (this.door) {
-      this.tenants.push(person);
-      console.log(`${person.name} увійшов у дім.`);
-    } else {
-      console.log("Двері зачинені. Ви не можете увійти.");
+    private key:Key
+    constructor(key:Key){
+        this.key=key
     }
-  }
-  abstract openDoor(key: Key): void;
+    getKey():Key{
+        return this.key
+    }
 }
+let key= new Key()
+let person=new Person(key)
+let house=new MyHouse(key)
+house.comeIn(person)
+house.openDoor(person.getKey())
+house.comeIn(person)
